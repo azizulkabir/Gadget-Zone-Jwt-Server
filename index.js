6f43cfdee46f3711c05100a056e0586cd809cf38
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-15 02:54:31 +0600

    finally fix the problem finally close

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 var cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const { json } = require('express');
-const app = express();
+const port = process.env.PORT || 5000;
 require('dotenv').config();
+const app = express();
+
+//middleware
 app.use(cors());
 app.use(express.json())
 
@@ -12,38 +14,39 @@ app.use(express.json())
 
 
 
-const port = process.env.PORT || 5000;
-
-
 //mongodb connect
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.crvvuvf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 async function run() {
    try {
-     await client.connect();
-     console.log('db connected');
+      await client.connect();
+      console.log('db connected');
+      const productCollection = client.db("gadgetZone").collection("products");
+
       //upload products
-      app.post('/uploadproduct', async(req, rea)=>{
-        
+      app.post('/upload', async (req, res) => {
+         const product = req.body;
+         console.log(product);
+         const result = await productCollection.insertOne(product);
+         console.log(result);
+         res.send(result);
       })
 
 
    } finally {
-     await client.close();
+      //await client.close();
    }
- }
- run().catch(console.dir);
+}
+run().catch(console.dir);
 
 //Api create 
+
 app.get('/', (req, res) => {
    res.send('My node is running......')
-})
-
-app.get('/user', (req, res) => {
-   res.send('this is user pages')
 })
 
 app.listen(port, () => {
