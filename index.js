1944d3e03ff99809821fce7093e54a53a1eb3adb
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-15 00:35:26 +0600

    mongodb connectes

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
- const express = require('express');
- const app = express()
- const port = process.env.PORT || 5000;
+const express = require('express');
+var cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+app.use(cors())
+
+// user: GadgetZone
+//password: Yw7JQCCeoSTBvHkO
 
 
- app.get('/', (req, res)=>{
-    res.send('My node is running......')
- })
 
- app.get('/user', (req, res)=>{
-    res.send('this is user pages')
- })
+const port = process.env.PORT || 5000;
 
- app.listen(port, ()=>{
-    console.log('my node start on the port ${port}')
- })
+
+app.get('/', (req, res) => {
+   res.send('My node is running......')
+})
+
+app.get('/user', (req, res) => {
+   res.send('this is user pages')
+})
+
+app.listen(port, () => {
+   console.log('my node start on the port ${port}')
+})
+
+//mongodb connect
+
+
+const uri = `mongodb+srv://GadgetZone:<password>@cluster0.crvvuvf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+   const collection = client.db("gadgetzone").collection("products");
+   // perform actions on the collection object
+   console.log('connected database');
+   client.close();
+});
